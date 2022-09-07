fcd946982a408f575a12f69904fdc9b7deaa7ace
Lespht <75142726+Lespht@users.noreply.github.com>
2022-09-07 07:50:37 -0600

    IB 0.1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="index.css" rel="stylesheet" type="text/css">
+        <meta charset="utf-8">
+        <title>login</title>
+</head>
+    <body>
+      <div class="login-page">
+      <div class="form">
+        <div class="login">
+          <div class="login-header">
+            <h3>LOGIN</h3>
+            <p>Please enter your credentials to login.</p>
+          </div>
+        </div>
+        <form method="post" action="validar.php" class="login-form">
+          <input id="user" name="user" type="text" placeholder="username"/>
+          <input id="password" name="password" type="password" placeholder="password"/>
+          <button type="submit">login</button>
+          <p class="message">Not registered? <a href="sign_up.php">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+  </body> 
+</html>
