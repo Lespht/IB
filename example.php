f9d669fef3686b180199d79faf5b2e27d1296aa3
Lespht <josep99a2@gmail.com>
2023-02-06 10:04:05 -0600

    uy

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 include 'Calendar.php';
-$calendar = new Calendar('2021-02-02');
+$calendar = new Calendar('2022-02-02');
 $calendar->add_event('Birthday', '2021-02-03', 1, 'green');
 $calendar->add_event('Doctors', '2021-02-04', 1, 'red');
 $calendar->add_event('Holiday', '2021-02-16', 7);
